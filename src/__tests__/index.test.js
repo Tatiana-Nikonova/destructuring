07d3f5ffb907a62025082647bb652f963ceaa746
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-12-18 23:38:08 +0300

    Initial

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,40 @@
+import dest from '../index';
+
+test(('function destructuring'), () => {
+  const obj = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(dest(obj)).toEqual(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,13 @@
+export default function dest(obj) {
+  const { special } = obj;
+  const arr = [];
+  for (let i = 0; i < special.length; i += 1) {
+    const {
+      id, name, icon, description = 'Описание недоступно',
+    } = special[i];
+    arr.push({
+      id, name, icon, description,
+    });
+  }
+  return arr;
+}
